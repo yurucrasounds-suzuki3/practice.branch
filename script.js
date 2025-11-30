a07f7ad9ff0cba5f95cd2b29ad2427c456fb34df
yurucrasounds-suzuki3 <yurucra.sounds@gmail.com>
2025-11-30 11:10:09 +0900

    create page

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+  const btn = document.getElementById('showTasks');
+  const tasks = document.getElementById('tasks');
+  btn && btn.addEventListener('click', ()=>{
+    if(tasks.classList.contains('hidden')){
+      tasks.classList.remove('hidden');
+      btn.textContent = '練習タスクを隠す';
+    } else {
+      tasks.classList.add('hidden');
+      btn.textContent = '練習タスクを表示';
+    }
+  });
+});
